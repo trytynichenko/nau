41811ef4ad0c5abb60445e8afcf7eafc96a6ca5d
Leonid Trytynichenko <leonid.trytynichenko@masterofcode.com>
2020-04-13 10:23:33 +0300

    remove unused var

--- computer_architecture/lab/lab2_opt8.c
+++ computer_architecture/lab/lab2_opt8.c
@@ -23,7 +23,6 @@ int main()
 {
     int fib[10] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
     int n = sizeof(fib)/sizeof(fib[0]);
-    float avg;
 
     for (int i = 0; i < n; i++)
         printf("Fibonacci number [%d] = %d\n", i, fib[i] );
